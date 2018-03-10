9137dfb6121dde031bd12b9bf764b90327c0823b
murray <murray@localhost.localdomain>
2018-03-10 16:54:01 +1300

    Add parses and pre-generated function text files

---
+++ code_check.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import re
+
+security_functions = []
+deprecat_functions = []
+unsafe_functions = []
+warning_functions = []
+pop_the_champagne_functions = []
+
+def create_lists():
+    global security_functions
+    global deprecat_functions
+    global unsafe_functions
+    global warning_functions
+    global pop_the_champagne_functions
+
+    fd = open("security.txt", "r")
+    temp = fd.readlines()
+    security_functions = [x.strip() for x in temp] #https://stackoverflow.com/questions/3277503/how-do-i-read-a-file-line-by-line-into-a-list
+    fd.close()
+
+    fd = open("deprecated.txt", "r")
+    temp = fd.readlines()
+    deprecat_functions = [x.strip() for x in temp]
+    fd.close()
+
+    fd = open("unsafe.txt", "r")
+    temp = fd.readlines()
+    unsafe_functions = [x.strip() for x in temp]
+    fd.close()
+
+    fd = open("warning.txt", "r")
+    temp = fd.readlines()
+    warning_functions = [x.strip() for x in temp]
+    fd.close
+
+    fd = open("code-exec.txt", "r")
+    temp = fd.readlines()
+    pop_the_champagne_functions = [x.strip() for x in temp]
+    fd.close()  
+ 
+def check_code():
+    try:
+        os.stat("output")
+    except:
+        os.mkdir("output")
+    security_found_fd = open("output/security_found.txt", "a")  
+    deprecated_found_fd = open("output/deprecated_found.txt", "a")
+    unsafe_found_fd = open("output/unsafe_found.txt", "a")
+    warning_found_fd = open("output/warning_found.txt", "a")
+    code_exec_found_fd = open("output/code_exec_found.txt", "a")
+    for filename in glob.iglob("SOURCE/**/*.php*", recursive=True): 
+        fd = open(filename, "r")
+        current_file = fd.read()
+
+        for item in security_functions:
+            temp = re.search(item, current_file, re.IGNORECASE)
+            if temp: 
+                security_found_fd.write("%s in %s\n" % (item, filename))
+       
+        for item in deprecat_functions:
+            temp = re.search(item, current_file, re.IGNORECASE)
+            if temp: 
+                deprecated_found_fd.write("%s in %s\n" % (item, filename))
+
+        for item in unsafe_functions:
+            temp = re.search(item, current_file, re.IGNORECASE)
+            if temp:
+                unsafe_found_fd.write("%s in %s\n" % (item, filename))
+
+        for item in warning_functions:
+            temp = re.search(item, current_file, re.IGNORECASE)
+            if temp:
+                warning_found_fd.write("%s in %s\n" % (item, filename))
+
+        for item in pop_the_champagne_functions:
+            temp = re.search(item, current_file, re.IGNORECASE)
+            if temp:
+                code_exec_found_fd.write("%s in %s\n" % (item, filename)) 
+        fd.close()
+
+    security_found_fd.close()
+    deprecated_found_fd.close()
+    unsafe_found_fd.close()
+    warning_found_fd.close()
+    code_exec_found_fd.close()
+
+def main():
+    create_lists()   
+    check_code()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parse_docs.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import re
+
+class Errata: 
+    def parse_docs(self): 
+        function_name_start = "docs/function."
+        function_name_end = ".html"
+        keywords = ["security", "deprecat", "unsafe", "warning", "code exec"] # changes here need updating below
+        security_matches = []
+        deprecat_matches = []
+        unsafe_matches = []
+        warning_matches = []
+        pop_the_champagne_matches = []
+        security_fd = open("security.txt", "a")
+        deprecat_fd = open("deprecated.txt", "a")
+        unsafe_fd = open("unsafe.txt", "a")
+        warning_fd = open("warning.txt", "a")
+        code_exec_fd = open("code-exec.txt", "a")
+        for filename in glob.iglob("docs/**/function.*.html", recursive=True): 
+            fd = open(filename, "r")
+            current_file = fd.read()
+            for item in keywords:
+                temp = re.search(item, current_file, re.IGNORECASE)
+                if temp:
+                    if item == "security":
+                        function_name = re.search('%s(.*)%s' % (function_name_start, function_name_end), filename).group(1)
+                        function_name = function_name.replace("-","_")
+                        security_matches.append(function_name)
+                    if item == "deprecat":
+                        function_name = re.search('%s(.*)%s' % (function_name_start, function_name_end), filename).group(1)
+                        function_name = function_name.replace("-","_") 
+                        deprecat_matches.append(function_name) 
+                    if item == "unsafe":
+                        function_name = re.search('%s(.*)%s' % (function_name_start, function_name_end), filename).group(1)
+                        function_name = function_name.replace("-","_")
+                        unsafe_matches.append(function_name)
+                    if item == "warning":
+                        function_name = re.search('%s(.*)%s' % (function_name_start, function_name_end), filename).group(1)
+                        function_name = function_name.replace("-","_")
+                        warning_matches.append(function_name)
+                    if item == "code exec":
+                        function_name = re.search('%s(.*)%s' % (function_name_start, function_name_end), filename).group(1)
+                        function_name = function_name.replace("-","_")
+                        pop_the_champagne_matches.append(function_name)
+            fd.close() 
+        if len(security_matches) != 0:
+            for x in security_matches:
+                security_fd.write("%s\n" % x)
+
+        if len(deprecat_matches) != 0:
+            for x in deprecat_matches:
+                deprecat_fd.write("%s\n" % x)
+
+        if len(unsafe_matches) != 0:
+            for x in unsafe_matches:
+                unsafe_fd.write("%s\n" % x)
+
+        if len(warning_matches) != 0:
+            for x in warning_matches:
+                warning_fd.write("%s\n" % x)
+
+        if len(pop_the_champagne_matches) != 0:
+            for x in pop_the_champagne_matches:
+               code_exec_fd.write("%s\n" % x)
+ 
+        security_fd.close()
+        deprecat_fd.close()
+        unsafe_fd.close()
+        warning_fd.close()
+        code_exec_fd.close()
+        
+def main():
+    x = Errata()
+    x.parse_docs()
+
+if __name__ == "__main__":
+    main()
